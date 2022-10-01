d8e3aac55f4142100b9889bb1ecfc93115205a7a
Jonthonel Requena <jonthonel.requena@soapros.pe>
2022-09-30 21:49:16 -0500

    feat: objetos en JS

---
+++ Frontend/JS-bluuweb/app.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded",() =>{
+//     console.log(document.getElementById("titulo").textContent);
+
+// })
+
+const h1 = document.getElementById("titulo")
+h1.textContent = "Nuevo text since JS"
+h1.style.backgroundColor= "red"
+h1.style.color="white"
+
+console.log(document.querySelectorAll(".container .text-danger"));
